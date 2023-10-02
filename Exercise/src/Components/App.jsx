b5284abed3bd329023b59822a20a7515b4f7a9d5
ll-sie <e.ndiramiye@alustudent.com>
2023-10-02 16:58:21 +0200

    1

---
+++ Exercise/src/Components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-amber-400 h-screen flex flex-col justify-center items-center'> 
+       <p>Hello world!</p>
+    </div>
+  )
+}
+
+export default App
+
