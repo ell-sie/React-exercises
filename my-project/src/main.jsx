9449d72f0ed1602011f31482c930e9ef5a9b4f23
ll-sie <e.ndiramiye@alustudent.com>
2023-10-09 11:08:01 +0200

    rendering jokes usng json

---
+++ my-project/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './App.css'
+import Exercise7 from './Components/Exercise7.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Exercise7 />
+  </React.StrictMode>,
+)
